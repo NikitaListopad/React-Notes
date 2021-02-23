ca51aa9b15daff7b64f84df61aa8e9256374a636
Nikita <listopad.nikita96@gmail.com>
2021-02-23 20:16:41 +0200

    delete no used reducers

--- src/store/categories/selectors.js
+++
@@ -1,4 +0,0 @@
-import {createSelector} from 'reselect';
-
-const categories = state => state.categoriesReducer
-export const categoriesSelector = createSelector(categories, categories => (categories))
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,18 @@
-import {createStore, applyMiddleware, combineReducers} from "redux";
+import {createStore, combineReducers} from "redux";
 import {notes} from "./notes";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import {categoriesReducer} from "./categories";
 
 
 const persistConfig = {
     key: 'notes',
     storage,
-    whitelist: ['notes', 'categoriesReducer']
+    whitelist: ['notes']
 }
 
 const reducers = combineReducers({
-    notes,
-    categoriesReducer
+    notes
 })
 
 const persistedReducer = persistReducer(persistConfig, reducers)
